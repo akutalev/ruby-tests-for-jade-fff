f2630b2d71129f860858dbd5c0c262a066d76659
Andrey Kutalev <akutalev@gmail.com>
2014-01-22 15:03:48 +0700

    Some tests for Jade-fff project.

---
+++ recruiter_pages.rb
@@ -0,0 +1,41 @@
+#!/bin/env ruby
+# encoding: utf-8
+require_relative 'spec/spec_helper'
+
+describe 'Recruiter System' do
+  before {
+    goto 'http://user:@recruiters.7bits.it/'
+  }
+
+  shared_examples_for 'login page' do
+    it do
+      text_field(name: 'j_username').should be_present
+      text_field(name: 'j_password').should be_present
+      button(name: 'submit')
+    end
+  end
+
+  context 'when login page' do
+    it_behaves_like 'login page'
+  end
+
+  context 'when recruiter logging in' do
+    it do
+      text_field(name: 'j_username').set 'recruiter'
+      text_field(name: 'j_password').set '123123'
+      button(name: 'submit').click
+      ul(class: 'nav navbar-nav navbar-right').should be_present
+    end
+  end
+
+  context 'when user logged out' do
+    it 'signs him off and redirect to login pages' do
+      a(id: 'menud').should be_present
+      a(id: 'menu').click
+      b = button(name: 'submit')
+      b.should be_present
+      b.click
+    end
+    it_behaves_like 'login page'
+  end
+end
